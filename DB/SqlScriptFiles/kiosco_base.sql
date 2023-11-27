b8630166bcd2d5d5895c9b221aaa9fc2482e1fe7
Matias Mutter <matimutter@gmail.com>
2023-11-27 14:12:35 -0300

    Update DB model

--- DB/SqlScriptFiles/kiosco_base.sql
+++ DB/SqlScriptFiles/kiosco_base.sql
@@ -3,26 +3,31 @@ create database if not exists kioscoDB;
 use kioscoDB;
 
 CREATE TABLE IF NOT EXISTS `usuario` (
-    `id` VARCHAR(20) NOT NULL UNIQUE,
+    `id_usuario` SMALLINT NOT NULL AUTO_INCREMENT,
+	`usuario` VARCHAR(20) NOT NULL UNIQUE,
     `clave` VARCHAR(30) NOT NULL,
     `ci` INT(8) UNSIGNED,
     `nombre` VARCHAR(45),
 	`apellido` VARCHAR(45),
     `es_admin` BOOLEAN NOT NULL,
-    `salario_por_hora` DECIMAL(15,2),
+    `salario_por_hora` DECIMAL(15,2) NOT NULL,
     `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP () ON UPDATE CURRENT_TIMESTAMP (),
-    `deshabilitado` BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (`id`)
+    `deshabilitado` BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (`id_usuario`)
     -- CONSTRAINT dsf 
     -- UNIQUE(sdfsdf,sdfsdf,Rfg)
 );
 
 CREATE TABLE IF NOT EXISTS `usuario_tel` (
-	`id` VARCHAR(20) NOT NULL,
+	`id_usuario` SMALLINT NOT NULL,
     `telefono` VARCHAR(20) NOT NULL,
-    PRIMARY KEY (`id`, `telefono`),
-    CONSTRAINT `fk_usuario_id`FOREIGN KEY (`id`) REFERENCES `usuario` (`id`)  ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (`id_usuario`, `telefono`),
+    CONSTRAINT `fk_usuario_id`FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)  ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS `horario` (
+	`dia` ENUM('L','M','X','J','V','S','D') NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS producto (
     prducto_id SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
