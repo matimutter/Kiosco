bf712ed51afc66685561702a491fbc311dcd7a2f
Matias Mutter <matimutter@gmail.com>
2023-12-05 17:00:32 -0300

    Update MER y DB (primera version)
    Primera versión de DB Funcional

--- DB/SqlScriptFiles/kiosco_base.sql
+++ DB/SqlScriptFiles/kiosco_base.sql
@@ -11,14 +11,15 @@ CREATE TABLE IF NOT EXISTS `usuario` (
     `apellido` VARCHAR(45),
     `es_admin` BOOLEAN NOT NULL,
     `salario_por_hora` DECIMAL(15 , 2 ) NOT NULL,
-    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deshabilitado` BOOLEAN NOT NULL DEFAULT FALSE,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_usuario`)
 );
 
 CREATE TABLE IF NOT EXISTS `usuario_tel` (
     `id_usuario` SMALLINT UNSIGNED NOT NULL,
     `telefono` VARCHAR(20) NOT NULL,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_usuario` , `telefono`),
     CONSTRAINT `fk_id_usuario_tel` FOREIGN KEY (`id_usuario`)
         REFERENCES `usuario` (`id_usuario`)
@@ -40,6 +41,7 @@ CREATE TABLE IF NOT EXISTS `turno` (
 CREATE TABLE IF NOT EXISTS `asigna` (
     `id_turno` TINYINT UNSIGNED NOT NULL,
     `id_usuario` SMALLINT UNSIGNED NOT NULL,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_turno` , `id_usuario`),
     CONSTRAINT `fk_id_turno_asigna` FOREIGN KEY (`id_turno`)
         REFERENCES `turno` (`id_turno`)
@@ -56,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `balance` (
     `fecha_cierre` DATETIME,
     `salida` DECIMAL(15 , 2 ) NOT NULL,
     `entrada` DECIMAL(15 , 2 ) NOT NULL,
-    `nota` VARCHAR(120),
+    `nota` VARCHAR(200),
     `estado` ENUM('ABIERTO', 'CERRADO', 'PENDIENTE', 'ANULADO') NOT NULL,
     `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_balance`),
@@ -72,24 +74,27 @@ CREATE TABLE IF NOT EXISTS `jornada` (
     `id_balance` SMALLINT UNSIGNED NOT NULL,
     `id_usuario` SMALLINT UNSIGNED NOT NULL,
     `paga_del_dia` DECIMAL(15 , 2 ) NOT NULL,
-    `detalle` VARCHAR(120),
+    `detalle` VARCHAR(200),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_jornada`),
     CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`)
         REFERENCES `asigna` (`id_usuario`)
         ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT `fk_id_balance` FOREIGN KEY (`id_usuario`)
+    CONSTRAINT `fk_id_balance` FOREIGN KEY (`id_balance`)
         REFERENCES `balance` (`id_balance`)
         ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT `key_jornada_unica` UNIQUE (`fecha` , `hora_entrada` , `hora_salida`)
 );
 
 CREATE TABLE IF NOT EXISTS `categoria` (
-    `nombre_categoria` VARCHAR(30) NOT NULL PRIMARY KEY
+    `nombre_categoria` VARCHAR(30) NOT NULL UNIQUE,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`nombre_categoria`)
 );
 
 CREATE TABLE IF NOT EXISTS `producto` (
     `id_producto` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `codigo` VARCHAR(30) NOT NULL,
+    `codigo` VARCHAR(30),
     `nombre` VARCHAR(60) NOT NULL,
     `categoria` VARCHAR(30),
     `precio_venta` DECIMAL(15 , 2 ) UNSIGNED NOT NULL,
@@ -97,9 +102,95 @@ CREATE TABLE IF NOT EXISTS `producto` (
     `fecha_terminado` DATETIME,
     `reponer` BOOLEAN DEFAULT FALSE,
     `estado` ENUM('Alta', 'Baja', 'Extraviado') DEFAULT 'Alta' NOT NULL,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_producto`),
     CONSTRAINT `fk_categoria_producto` FOREIGN KEY (`categoria`)
         REFERENCES `categoria` (`nombre_categoria`)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS `boleta` (
+    `id_boleta` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id_usuario` SMALLINT UNSIGNED NOT NULL,
+    `monto_total` DECIMAL(15 , 2 ) UNSIGNED NOT NULL,
+    `monto_entrega` DECIMAL(15 , 2 ) UNSIGNED NOT NULL,
+    `monto_cambio` DECIMAL(15 , 2 ) UNSIGNED NOT NULL,
+    `fecha` DATETIME,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id_boleta`),
+    CONSTRAINT `fk_id_usuario_boleta` FOREIGN KEY (`id_usuario`)
+        REFERENCES `usuario` (`id_usuario`)
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `venta` (
+    `id_boleta` INT UNSIGNED NOT NULL,
+    `id_producto` INT UNSIGNED NOT NULL,
+    `cantidad` TINYINT NOT NULL DEFAULT 1,
+    `precio_final_unitario` DECIMAL(15 , 2 ) NOT NULL,
+    `ganancia_total` DECIMAL(15 , 2 ) NOT NULL,
+    `id_balance` SMALLINT UNSIGNED NOT NULL,
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id_boleta` , `id_producto`),
+    CONSTRAINT `fk_id_boleta_venta` FOREIGN KEY (`id_boleta`)
+        REFERENCES `boleta` (`id_boleta`),
+	CONSTRAINT `fk_id_producto_venta` FOREIGN KEY (`id_producto`)
+		REFERENCES `producto`(`id_producto`),
+    CONSTRAINT `fk_id_balance_venta` FOREIGN KEY (`id_balance`)
+        REFERENCES `balance` (`id_balance`)
+);
+
+CREATE TABLE `proveedor` (
+    `proveedor` VARCHAR(30) NOT NULL UNIQUE,
+    `nota` VARCHAR(200),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`proveedor`)
+);
+
+CREATE TABLE `proveedor_tel`(
+    `proveedor` VARCHAR(30),
+    `telefono` VARCHAR(20),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`proveedor`, `telefono`),
+    FOREIGN KEY (`proveedor`) REFERENCES `proveedor`(`proveedor`)
+);
+
+CREATE TABLE `provee` (
+    `id_producto` INT UNSIGNED NOT NULL,
+    `proveedor` VARCHAR(30) NOT NULL UNIQUE,
+    `fecha_ingreso` DATE,
+    `vencimiento` DATE,
+    `costo_compra` DECIMAL(15 , 2 ) NOT NULL,
+    `precio_sugerido` DECIMAL(15 , 2 ),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id_producto`, `proveedor`),
+    FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`),
+    FOREIGN KEY (`proveedor`) REFERENCES `proveedor`(`proveedor`)
+);
+
+CREATE TABLE `deuda` (
+    `fecha` DATE,
+    `nombre` VARCHAR(20),
+    `id_boleta` INT UNSIGNED NOT NULL,
+	`id_balance` SMALLINT UNSIGNED NOT NULL,
+    `monto` DECIMAL(15, 2),
+    `concepto` VARCHAR(200),
+    `estado` ENUM('ABIERTA','CERRADA','PAGA'),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`fecha`, `nombre`),
+    FOREIGN KEY (`id_boleta`) REFERENCES `boleta`(`id_boleta`),
+    FOREIGN KEY (`id_balance`) REFERENCES `balance`(`id_balance`)
+);
+
+CREATE TABLE `entrada_salida` (
+    `id_ent_sal` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id_balance` SMALLINT UNSIGNED NOT NULL,
+    `fecha` DATE NOT NULL,
+    `concepto` VARCHAR(30) NOT NULL,
+    `tipo` ENUM('ENTRADA', 'SALIDA') NOT NULL,
+    `monto` DECIMAL(15, 2) NOT NULL,
+    `detalle` VARCHAR(200),
+    `ultima_actividad` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id_ent_sal`),
+    FOREIGN KEY (`id_balance`) REFERENCES Balance(`id_balance`)
+);
